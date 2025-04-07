3146f128d26c2c9a5a89299e8cf968a94df5eaf8
mayara <mayaracastrosilva89@gmail.com>
2025-04-07 09:47:46 -0300

    atividade

---
+++ aula04.py
@@ -0,0 +1,31 @@
+
+mes=  int(input("qual é o mês do seu nascimento"))
+if mes == 1:
+    print("janeiro")
+elif mes == 2:
+    print("fevereiro")
+elif mes == 3:
+    print("março")
+elif mes == 4:
+    print("abril")
+elif mes == 5:
+    print("maio")
+elif mes == 6:
+    print ("junho")
+elif mes == 7:
+    print ("julho")
+elif mes == 8:
+    print("agosto")
+elif mes == 9:
+    print("setembro")
+elif mes == 10:
+    print("outubro")
+elif mes == 11:
+    print("novembro")
+elif mes == 12:
+    print("dezembro")
+else:
+    print("mes invalido")
+
+
+
